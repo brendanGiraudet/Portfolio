0e6f7c993bff6ebf611c9af985be74763e22fd7f
brendanGiraudet <brendan.giraudet1@gmail.com>
2024-02-12 10:07:57 +0100

    #2 update navmenu mobile version

--- Portfolio.App/Components/Layout/NavMenu.razor.cs
+++ Portfolio.App/Components/Layout/NavMenu.razor.cs
@@ -6,6 +6,10 @@ public partial class NavMenu
 {
     private string _applicationVersion = string.Empty;
 
+    private bool _hidden = true;
+
+    private string _hiddenClass => _hidden ? "hidden": string.Empty;
+
     [Inject] public IConfiguration? Configuration { get; set; }
 
     protected override void OnInitialized()
@@ -13,4 +17,6 @@ public partial class NavMenu
         base.OnInitialized();
         _applicationVersion = Configuration?["ApplicationVersion"] ?? "1.0.0";
     }
+
+    private void Toogle() => _hidden = !_hidden;
 }
